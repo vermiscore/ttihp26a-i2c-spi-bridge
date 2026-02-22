060cf5818d42bb9e8bbb06418727d2c20df54905
vermiscore <vermiscore@users.noreply.github.com>
2026-02-22 05:20:46 -0800

    Fix info.yaml format and tb.v module name

--- test/tb.v
+++ test/tb.v
@@ -23,8 +23,8 @@ module tb ();
   wire [7:0] uio_out;
   wire [7:0] uio_oe;
 
-  // Replace tt_um_example with your module name:
-  tt_um_example user_project (
+  // Replace tt_um_vermiscore_i2c_spi_bridge with your module name:
+  tt_um_vermiscore_i2c_spi_bridge user_project (
       .ui_in  (ui_in),    // Dedicated inputs
       .uo_out (uo_out),   // Dedicated outputs
       .uio_in (uio_in),   // IOs: Input path
